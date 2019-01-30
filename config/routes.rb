9f612dce4c04e1d215b654506b1883744197f184
abdulmukmin <abdmukmin@rocketmail.com>
2019-01-30 09:28:20 +0700

    try to fixed processing as HTML

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  resources :tasks
-  resources :users
+  resources :tasks, defaults: {format: :json}
+  resources :users, defaults: {format: :json}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post 'authenticate', to: 'authentication#authenticate'
+  post 'authenticate', to: 'authentication#authenticate', defaults: {format: :json}
   
-  get 'tasks/duedates/asc', to: 'tasks#index_duedates_asc'
-  get 'tasks/duedates/desc', to: 'tasks#index_duedates_desc'
-  get 'tasks/description/asc', to: 'tasks#index_description_asc'
-  get 'tasks/description/desc', to: 'tasks#index_description_desc'
-  get 'tasks/priority/asc', to: 'tasks#index_priority_asc'
-  get 'tasks/priority/desc', to: 'tasks#index_priority_desc'
+  get 'tasks/duedates/asc', to: 'tasks#index_duedates_asc', defaults: {format: :json}
+  get 'tasks/duedates/desc', to: 'tasks#index_duedates_desc', defaults: {format: :json}
+  get 'tasks/description/asc', to: 'tasks#index_description_asc', defaults: {format: :json}
+  get 'tasks/description/desc', to: 'tasks#index_description_desc', defaults: {format: :json}
+  get 'tasks/priority/asc', to: 'tasks#index_priority_asc', defaults: {format: :json}
+  get 'tasks/priority/desc', to: 'tasks#index_priority_desc', defaults: {format: :json}
 end
